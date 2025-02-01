11906a3aa1e0e5560b48f79901299ef778cd3258
Colin O'Curran <colinocurran2027@lschs.org>
2025-02-01 14:26:54 -0500

    More stuff!
    Incomplete -- added limit switch code (limit switch == true might be wrong)

--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -16,6 +16,7 @@ import com.ctre.phoenix6.hardware.TalonFX;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
 
+
 import com.playingwithfusion.*;
 
 
@@ -26,9 +27,10 @@ public class ElevatorSubsystem extends SubsystemBase {
   /** Creates a new ElevatorSubsystem. TalonFX motor will be final, SparkMax used for testing */
   //private final TalonFX m_elevatorMotor;
   private final SparkMax m_elevatorMotor;
-    
+  public boolean limitSwitch = false;  
   private final ElevatorFeedforward m_elevatorFeedforward = new ElevatorFeedforward(ElevatorConstants.kS, ElevatorConstants.kG, ElevatorConstants.kV, ElevatorConstants.kA);
   private final PIDController m_elevatorFeedback = new PIDController(ElevatorConstants.kP, ElevatorConstants.kI, ElevatorConstants.kD);
+  public DigitalInput elevatorLimit;
   public ElevatorSubsystem() {
     //m_elevatorMotor = new TalonFX(ElevatorConstants.kMotorID);
     m_elevatorMotor = new SparkMax(ElevatorConstants.kMotorID, MotorType.kBrushless);
@@ -44,13 +46,25 @@ public class ElevatorSubsystem extends SubsystemBase {
               m_elevatorMotor.get(), setpointRotationsPerSecond));
       });
     waitUntil(m_elevatorFeedback::atSetpoint).andThen(() -> m_elevatorMotor.set(1));
-    //TO DO: add code to reset the motor's encoder every time the elevator passes a specific value
+    if (limitSwitch == true){
+      m_elevatorMotor.set(0);
     }
+    }
+  public boolean elevatorPosition(){
+    if (elevatorLimit.get()){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
  
 
     @Override
+    
     public void periodic() {
       // This method will be called once per scheduler run
+      
     }
   }
 
