61ae84be2836b8904ee323d893ad44fc267da81f
StauferM <123831985+StauferM@users.noreply.github.com>
2025-02-02 15:49:08 -0500

    Added Base LEDSubsytem
    Added LED Subsystem and LEDConstants  sub-class in Constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -16,4 +16,20 @@ public final class Constants {
     public static final double kD = 0.1;  // Moderate dampening
     public static final double kTolerance = 0.05;
   }
+  public static class LEDConstants{
+    public static final int LEDDriverOneID = 3;
+    public static final double colorRed = 0.61;
+    public static final double colorHotPink = 0.57;
+    public static final double colorYellow = 0.69;
+    public static final double colorSkyBlue = 0.83;
+    public static final double colorBlueViolet = 0.89;
+    public static final double colorWhite = 0.93;
+    public static final double colorLimeGreen = 0.73;
+    public static final double colorOrange = 0.65;
+    public static final double colorDarkGreen = 0.75;
+    public static final double colorLawnGreen = 0.71;
+    public static final double colorBlue = 0.87;
+    public static final double colorGold = 0.67;
+    public static final double twinklesColorOneAndTwo = 0.51;
+  }
 }
